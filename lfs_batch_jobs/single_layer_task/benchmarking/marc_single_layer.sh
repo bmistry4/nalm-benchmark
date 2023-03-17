aea1296ca968e72b6193e412866bc2f30212d355
Bhumika <bm4g15@soton.ac.uk>
2023-03-17 16:11:05 +0000

    update shell with MulMCFCSignINALU and MulMCFCSignRealNPU runs (ids 34 and  35)

--- lfs_batch_jobs/single_layer_task/benchmarking/marc_single_layer.sh
+++ lfs_batch_jobs/single_layer_task/benchmarking/marc_single_layer.sh
@@ -38,16 +38,40 @@ for ((i=0;i<${#interpolation_ranges[@]};++i))
 #    > /data/bm4g15/nalu-stable-exp/logs/${experiment_name}/${interpolation_ranges[i]}-${seed}.out \
 #    2> /data/bm4g15/nalu-stable-exp/logs/${experiment_name}/errors/${interpolation_ranges[i]}-${seed}.err # do not send to bkg. A single run takes up most of the cpu resources. 
 
-    experiment_name='benchmark/sltr-in2/add/MCFC'
+#    experiment_name='benchmark/sltr-in2/add/MCFC'
+#    mkdir -p /data/bm4g15/nalu-stable-exp/logs/${experiment_name}/errors
+#    python3 -u /home/bm4g15/nalm-benchmark/experiments/single_layer_benchmark.py \
+#    --id 33 --operation add --layer-type MCFC \
+#    --regualizer-scaling-start 20000 --regualizer-scaling-end 35000 \
+#    --interpolation-range ${interpolation_ranges[i]} --extrapolation-range ${extrapolation_ranges[i]} \
+#    --seed ${seed} --max-iterations 50000 ${verbose_flag} --log-interval ${log_interval} \
+#    --name-prefix ${experiment_name} --remove-existing-data --no-cuda ${no_save_flag} \
+#    > /data/bm4g15/nalu-stable-exp/logs/${experiment_name}/${interpolation_ranges[i]}-${seed}.out \
+#    2> /data/bm4g15/nalu-stable-exp/logs/${experiment_name}/errors/${interpolation_ranges[i]}-${seed}.err # do not send to bkg. A single run takes up most of the cpu resources.
+
+    experiment_name='benchmark/sltr-in2/mul/MulMCFCSignINALU'
     mkdir -p /data/bm4g15/nalu-stable-exp/logs/${experiment_name}/errors
     python3 -u /home/bm4g15/nalm-benchmark/experiments/single_layer_benchmark.py \
-    --id 33 --operation add --layer-type MCFC \
+    --id 34 --operation mul --layer-type MulMCFCSignINALU \
     --regualizer-scaling-start 20000 --regualizer-scaling-end 35000 \
     --interpolation-range ${interpolation_ranges[i]} --extrapolation-range ${extrapolation_ranges[i]} \
     --seed ${seed} --max-iterations 50000 ${verbose_flag} --log-interval ${log_interval} \
     --name-prefix ${experiment_name} --remove-existing-data --no-cuda ${no_save_flag} \
     > /data/bm4g15/nalu-stable-exp/logs/${experiment_name}/${interpolation_ranges[i]}-${seed}.out \
-    2> /data/bm4g15/nalu-stable-exp/logs/${experiment_name}/errors/${interpolation_ranges[i]}-${seed}.err # do not send to bkg. A single run takes up most of the cpu resources. 
+    2> /data/bm4g15/nalu-stable-exp/logs/${experiment_name}/errors/${interpolation_ranges[i]}-${seed}.err # do not send to bkg. A single run takes up most of the cpu resources.
+
+#    experiment_name='benchmark/sltr-in2/mul/MulMCFCSignRealNPU'
+#    mkdir -p /data/bm4g15/nalu-stable-exp/logs/${experiment_name}/errors
+#    python3 -u /home/bm4g15/nalm-benchmark/experiments/single_layer_benchmark.py \
+#    --id 35 --operation mul --layer-type MulMCFCSignRealNPU \
+#    --regualizer-scaling-start 20000 --regualizer-scaling-end 35000 \
+#    --reg-scale-type madsen --regualizer-gate 1 \
+#    --interpolation-range ${interpolation_ranges[i]} --extrapolation-range ${extrapolation_ranges[i]} \
+#    --seed ${seed} --max-iterations 50000 ${verbose_flag} --log-interval ${log_interval} \
+#    --name-prefix ${experiment_name} --remove-existing-data --no-cuda ${no_save_flag} \
+#    > /data/bm4g15/nalu-stable-exp/logs/${experiment_name}/${interpolation_ranges[i]}-${seed}.out \
+#    2> /data/bm4g15/nalu-stable-exp/logs/${experiment_name}/errors/${interpolation_ranges[i]}-${seed}.err # do not send to bkg. A single run takes up most of the cpu resources.
+
 
   done
   wait
